6c30aa566063709c73f6578081ee18d05d6e1e75
Koichi ITO <koic.ito@gmail.com>
2018-07-11 13:26:31 +0900

    Create gemspec for initial version
    This PR adds gemspec for initial version.
    
    The initial version was set to 0.5.0.
    That's because rubocop-rails 0.9 or lower was used up to 0.4.1.
    https://rubygems.org/gems/rubocop-rails/versions

---
+++ lib/rubocop/rails/version.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Rails
+    # This module holds the RuboCop Rails version information.
+    module Version
+      STRING = '0.5.0'.freeze
+    end
+  end
+end
