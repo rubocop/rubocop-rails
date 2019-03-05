9b8335b11c7b546908469c25304a287a3301752f
Koichi ITO <koic.ito@gmail.com>
2019-03-05 19:06:09 +0900

    Add inject mechanism
    This PR imports inject mechanism based on RuboCop RSpec.
    https://github.com/rubocop-hq/rubocop-rspec/blob/v1.32.0/lib/rubocop/rspec/inject.rb
    
    Refer https://github.com/rubocop-hq/rubocop/issues/5976#issuecomment-433828766.

--- lib/rubocop-rails.rb
+++ lib/rubocop-rails.rb
@@ -3,5 +3,10 @@
 require 'rubocop'
 require 'rack/utils'
 
+require_relative 'rubocop/rails'
 require_relative 'rubocop/rails/version'
+require_relative 'rubocop/rails/inject'
+
+RuboCop::Rails::Inject.defaults!
+
 require_relative 'rubocop/cop/rails_cops'
