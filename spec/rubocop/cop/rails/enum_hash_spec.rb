2c0b6aff4fdbf858882d3c359833ef76e50e0ed9
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-07-26 12:02:51 +0900

    Fix false negatives for Rails/EnumHash cop

--- spec/rubocop/cop/rails/enum_hash_spec.rb
+++ spec/rubocop/cop/rails/enum_hash_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe RuboCop::Cop::Rails::EnumHash do
       it 'registers an offense' do
         expect_offense(<<~RUBY)
           enum status: %i[active archived]
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
+                       ^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
         RUBY
       end
     end
@@ -19,7 +19,7 @@ RSpec.describe RuboCop::Cop::Rails::EnumHash do
       it 'registers an offense' do
         expect_offense(<<~RUBY)
           enum status: %w[active archived]
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
+                       ^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
         RUBY
       end
     end
@@ -28,7 +28,7 @@ RSpec.describe RuboCop::Cop::Rails::EnumHash do
       it 'registers an offense' do
         expect_offense(<<~RUBY)
           enum status: %i(active archived)
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
+                       ^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
         RUBY
       end
     end
@@ -37,7 +37,7 @@ RSpec.describe RuboCop::Cop::Rails::EnumHash do
       it 'registers an offense' do
         expect_offense(<<~RUBY)
           enum status: %w(active archived)
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
+                       ^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
         RUBY
       end
     end
@@ -46,7 +46,36 @@ RSpec.describe RuboCop::Cop::Rails::EnumHash do
       it 'registers an offense' do
         expect_offense(<<~RUBY)
           enum status: [:active, :archived]
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
+                       ^^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
+        RUBY
+      end
+    end
+
+    context 'when the enum name is a string' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY)
+          enum "status" => %i[active archived]
+                           ^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
+        RUBY
+      end
+    end
+
+    context 'when the enum name is not a literal' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY)
+          enum KEY => %i[active archived]
+                      ^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `KEY` enum declaration. Use hash syntax instead.
+        RUBY
+      end
+    end
+
+    context 'with multiple enum definition for a `enum` method call' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY)
+          enum status: %i[active archived],
+                       ^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `status` enum declaration. Use hash syntax instead.
+               role: %i[owner member guest]
+                     ^^^^^^^^^^^^^^^^^^^^^^ Enum defined as an array found in `role` enum declaration. Use hash syntax instead.
         RUBY
       end
     end
