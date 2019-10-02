1a930968668b35d034e81852e5512b421c870a0e
Koichi ITO <koic.ito@gmail.com>
2019-10-02 10:10:57 +0200

    Restyle example formatting for `Rails/SafeNavigation`
    This PR is a change of document format for `Rails/SafeNavigation`.

--- lib/rubocop/cop/rails/safe_navigation.rb
+++ lib/rubocop/cop/rails/safe_navigation.rb
@@ -6,38 +6,36 @@ module RuboCop
       # This cop converts usages of `try!` to `&.`. It can also be configured
       # to convert `try`. It will convert code to use safe navigation.
       #
-      # @example
-      #   # ConvertTry: false
-      #     # bad
-      #     foo.try!(:bar)
-      #     foo.try!(:bar, baz)
-      #     foo.try!(:bar) { |e| e.baz }
+      # @example ConvertTry: false (default)
+      #   # bad
+      #   foo.try!(:bar)
+      #   foo.try!(:bar, baz)
+      #   foo.try!(:bar) { |e| e.baz }
       #
-      #     foo.try!(:[], 0)
+      #   foo.try!(:[], 0)
       #
-      #     # good
-      #     foo.try(:bar)
-      #     foo.try(:bar, baz)
-      #     foo.try(:bar) { |e| e.baz }
+      #   # good
+      #   foo.try(:bar)
+      #   foo.try(:bar, baz)
+      #   foo.try(:bar) { |e| e.baz }
       #
-      #     foo&.bar
-      #     foo&.bar(baz)
-      #     foo&.bar { |e| e.baz }
+      #   foo&.bar
+      #   foo&.bar(baz)
+      #   foo&.bar { |e| e.baz }
       #
+      # @example ConvertTry: true
+      #   # bad
+      #   foo.try!(:bar)
+      #   foo.try!(:bar, baz)
+      #   foo.try!(:bar) { |e| e.baz }
+      #   foo.try(:bar)
+      #   foo.try(:bar, baz)
+      #   foo.try(:bar) { |e| e.baz }
       #
-      #   # ConvertTry: true
-      #     # bad
-      #     foo.try!(:bar)
-      #     foo.try!(:bar, baz)
-      #     foo.try!(:bar) { |e| e.baz }
-      #     foo.try(:bar)
-      #     foo.try(:bar, baz)
-      #     foo.try(:bar) { |e| e.baz }
-      #
-      #     # good
-      #     foo&.bar
-      #     foo&.bar(baz)
-      #     foo&.bar { |e| e.baz }
+      #   # good
+      #   foo&.bar
+      #   foo&.bar(baz)
+      #   foo&.bar { |e| e.baz }
       class SafeNavigation < Cop
         include RangeHelp
 
