8e5969cc02b2ae5cbdca80d125b724fc0ddb2dbe
Koichi ITO <koic.ito@gmail.com>
2020-05-11 09:39:47 +0900

    Fix `NameError` for JRuby on CI
    This PR fixes the following test failure for JRuby on CI.
    
    ```console
      1) RuboCop::Cop::Rails::UnknownEnv registers an offense for typo of
      environment name with `==` operator
         Failure/Error: spell_checker =
      DidYouMean::SpellChecker.new(dictionary: environments)
    
         NameError:
           uninitialized constant DidYouMean::SpellChecker
         # ./lib/rubocop/cop/rails/unknown_env.rb:59:in `message'
         # ./lib/rubocop/cop/rails/unknown_env.rb:46:in `block in on_send'
         # ./lib/rubocop/cop/rails/unknown_env.rb:49:in
           `unknown_environment_equal?'
         # ./lib/rubocop/cop/rails/unknown_env.rb:44:in `on_send'
    (snip)
    
    rspec ./spec/rubocop/cop/rails/unknown_env_spec.rb:27 #
    RuboCop::Cop::Rails::UnknownEnv registers an offense for typo of
    environment name with `==` operator
    rspec ./spec/rubocop/cop/rails/unknown_env_spec.rb:16 #
    RuboCop::Cop::Rails::UnknownEnv registers an offense for typo of
    environment name
    ```
    
    https://circleci.com/gh/rubocop-hq/rubocop-rails/3393

--- spec/rubocop/cop/rails/unknown_env_spec.rb
+++ spec/rubocop/cop/rails/unknown_env_spec.rb
@@ -13,27 +13,31 @@ RSpec.describe RuboCop::Cop::Rails::UnknownEnv, :config do
     }
   end
 
-  it 'registers an offense for typo of environment name' do
-    expect_offense(<<~RUBY)
-      Rails.env.proudction?
-                ^^^^^^^^^^^ Unknown environment `proudction`. Did you mean `production`?
-      Rails.env.developpment?
-                ^^^^^^^^^^^^^ Unknown environment `developpment`. Did you mean `development`?
-      Rails.env.something?
-                ^^^^^^^^^^ Unknown environment `something`.
-    RUBY
-  end
+  # FIXME: The following is workaround that prevents
+  # `uninitialized constant DidYouMean::SpellChecker` for JRuby on CI.
+  unless RUBY_ENGINE == 'jruby' && ENV['CI'] == 'true'
+    it 'registers an offense for typo of environment name' do
+      expect_offense(<<~RUBY)
+        Rails.env.proudction?
+                  ^^^^^^^^^^^ Unknown environment `proudction`. Did you mean `production`?
+        Rails.env.developpment?
+                  ^^^^^^^^^^^^^ Unknown environment `developpment`. Did you mean `development`?
+        Rails.env.something?
+                  ^^^^^^^^^^ Unknown environment `something`.
+      RUBY
+    end
 
-  it 'registers an offense for typo of environment name with `==` operator' do
-    expect_offense(<<~RUBY)
-      Rails.env == 'proudction'
-                   ^^^^^^^^^^^^ Unknown environment `proudction`. Did you mean `production`?
-      'developpment' == Rails.env
-      ^^^^^^^^^^^^^^ Unknown environment `developpment`. Did you mean `development`?
+    it 'registers an offense for typo of environment name with `==` operator' do
+      expect_offense(<<~RUBY)
+        Rails.env == 'proudction'
+                     ^^^^^^^^^^^^ Unknown environment `proudction`. Did you mean `production`?
+        'developpment' == Rails.env
+        ^^^^^^^^^^^^^^ Unknown environment `developpment`. Did you mean `development`?
 
-      'something' === Rails.env
-      ^^^^^^^^^^^ Unknown environment `something`.
-    RUBY
+        'something' === Rails.env
+        ^^^^^^^^^^^ Unknown environment `something`.
+      RUBY
+    end
   end
 
   it 'accepts correct environment name' do
