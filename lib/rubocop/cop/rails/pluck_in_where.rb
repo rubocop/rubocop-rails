f0a5fbce8a4781a7bff7ac6b4676f19c8ad02712
fatkodima <fatkodima123@gmail.com>
2020-06-26 18:56:22 +0300

    Add new `Rails/PluckInWhere` cop

---
+++ lib/rubocop/cop/rails/pluck_in_where.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Rails
+      # This cop identifies places where `pluck` is used in `where` query methods
+      # and can be replaced with `select`.
+      #
+      # Since `pluck` is an eager method and hits the database immediately,
+      # using `select` helps to avoid additional database queries.
+      #
+      # @example
+      #   # bad
+      #   Post.where(user_id: User.active.pluck(:id))
+      #
+      #   # good
+      #   Post.where(user_id: User.active.select(:id))
+      #
+      class PluckInWhere < Cop
+        MSG = 'Use `select` instead of `pluck` within `where` query method.'
+        WHERE_METHODS = %i[where rewhere].freeze
+
+        def on_send(node)
+          add_offense(node, location: :selector) if node.method?(:pluck) && in_where?(node)
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.replace(node.loc.selector, 'select')
+          end
+        end
+
+        private
+
+        def in_where?(node)
+          send_node = node.each_ancestor(:send).first
+          send_node && WHERE_METHODS.include?(send_node.method_name)
+        end
+      end
+    end
+  end
+end
