e427e99c69b1ef539406dca47b1a2afbe84a8008
fatkodima <fatkodima123@gmail.com>
2020-06-29 17:45:37 +0300

    Add new `Rails/FindById` cop

---
+++ spec/rubocop/cop/rails/find_by_id_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::FindById do
+  subject(:cop) { described_class.new }
+
+  it 'registers an offense and corrects when using `where(id: ...).take!`' do
+    expect_offense(<<~RUBY)
+      User.where(id: 1).take!
+           ^^^^^^^^^^^^^^^^^^ Use `find(1)` instead of `where(id: 1).take!`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      User.find(1)
+    RUBY
+  end
+
+  it 'registers an offense and corrects when using `find_by_id!`' do
+    expect_offense(<<~RUBY)
+      User.find_by_id!(1)
+           ^^^^^^^^^^^^^^ Use `find(1)` instead of `find_by_id!(1)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      User.find(1)
+    RUBY
+  end
+
+  it 'registers an offense and corrects when using `find_by!(id: ...)`' do
+    expect_offense(<<~RUBY)
+      User.find_by!(id: 1)
+           ^^^^^^^^^^^^^^^ Use `find(1)` instead of `find_by!(id: 1)`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      User.find(1)
+    RUBY
+  end
+
+  it 'does not register an offense when using `find`' do
+    expect_no_offenses(<<~RUBY)
+      User.find(1)
+    RUBY
+  end
+end
