f0e9127eaab8d93bc4785b00a1c30bb41fbab243
fatkodima <fatkodima123@gmail.com>
2020-06-29 11:30:39 +0300

    Add new `Rails/MailerName` cop

---
+++ spec/rubocop/cop/rails/mailer_name_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::MailerName do
+  subject(:cop) { described_class.new }
+
+  ['ActionMailer::Base', 'ApplicationMailer'].each do |base|
+    context 'when regular class definition' do
+      it 'registers an offense and corrects when name without suffix' do
+        expect_offense(<<~RUBY)
+          class User < #{base}
+                ^^^^ Mailer should end with `Mailer` suffix.
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          class UserMailer < #{base}
+          end
+        RUBY
+      end
+
+      it 'does not register an offense when name with suffix' do
+        expect_no_offenses(<<~RUBY)
+          class UserMailer < #{base}
+          end
+        RUBY
+      end
+    end
+
+    context 'when `Class.new` definition' do
+      it 'registers an offense and corrects when name without suffix' do
+        expect_offense(<<~RUBY)
+          User = Class.new(#{base})
+          ^^^^ Mailer should end with `Mailer` suffix.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          UserMailer = Class.new(#{base})
+        RUBY
+      end
+
+      it 'does not register an offense when name with suffix' do
+        expect_no_offenses(<<~RUBY)
+          UserMailer = Class.new(#{base})
+        RUBY
+      end
+    end
+  end
+end
