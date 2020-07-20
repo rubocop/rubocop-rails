82f3cf5c84624c9311c57a046596970ad85fba17
Tejas Bubane <tejasbubane@gmail.com>
2020-07-20 22:55:06 +0530

    Add documentation for `Environments` config variable of Rails/UnknownEnv

--- lib/rubocop/cop/rails/unknown_env.rb
+++ lib/rubocop/cop/rails/unknown_env.rb
@@ -5,6 +5,9 @@ module RuboCop
     module Rails
       # This cop checks that environments called with `Rails.env` predicates
       # exist.
+      # By default the cop allows three environments which Rails ships with:
+      # `development`, `test`, and `production`.
+      # More can be added to the `Environments` config parameter.
       #
       # @example
       #   # bad
