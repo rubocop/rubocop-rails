9e00fdabd7588fdb595101d132411fd48d02c634
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2020-09-02 16:37:15 -0400

    [Fix #341] Add EnforcedStyle to Rails/WhereExists to allow where(...).exists? to be the enforced style

--- spec/rubocop/cop/rails/where_exists_spec.rb
+++ spec/rubocop/cop/rails/where_exists_spec.rb
@@ -1,50 +1,161 @@
 # frozen_string_literal: true
 
-RSpec.describe RuboCop::Cop::Rails::WhereExists do
-  subject(:cop) { described_class.new }
+RSpec.describe RuboCop::Cop::Rails::WhereExists, :config do
+  subject(:cop) { described_class.new(config) }
 
-  it 'registers an offense and corrects when using `where(...).exists?` with hash argument' do
-    expect_offense(<<~RUBY)
-      User.where(name: 'john').exists?
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `exists?(name: 'john')` over `where(name: 'john').exists?`.
-    RUBY
+  context 'when EnforcedStyle is "exists"' do
+    let(:cop_config) { { 'EnforcedStyle' => 'exists' } }
 
-    expect_correction(<<~RUBY)
-      User.exists?(name: 'john')
-    RUBY
+    it 'registers an offense and corrects when using `where(...).exists?` with hash argument' do
+      expect_offense(<<~RUBY)
+        User.where(name: 'john').exists?
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `exists?(name: 'john')` over `where(name: 'john').exists?`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        User.exists?(name: 'john')
+      RUBY
+    end
+
+    it 'registers an offense and corrects when using `where(...).exists?` with array argument' do
+      expect_offense(<<~RUBY)
+        User.where(['name = ?', 'john']).exists?
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `exists?(['name = ?', 'john'])` over `where(['name = ?', 'john']).exists?`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        User.exists?(['name = ?', 'john'])
+      RUBY
+    end
+
+    it 'registers an offense and corrects when using `where(...).exists?` with multiple arguments' do
+      expect_offense(<<~RUBY)
+        User.where('name = ?', 'john').exists?
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `exists?(['name = ?', 'john'])` over `where('name = ?', 'john').exists?`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        User.exists?(['name = ?', 'john'])
+      RUBY
+    end
+
+    it 'registers an offense when using `where(...).exists?` with an association' do
+      expect_offense(<<~RUBY)
+        user.posts.where(published: true).exists?
+                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `exists?(published: true)` over `where(published: true).exists?`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        user.posts.exists?(published: true)
+      RUBY
+    end
+
+    it 'does not register an offense when using `where(...).exists?` with string argument' do
+      expect_no_offenses(<<~RUBY)
+        User.where("name = 'john'").exists?
+      RUBY
+    end
+
+    it 'does not register an offense when using `exists?`' do
+      expect_no_offenses(<<~RUBY)
+        User.exists?(name: 'john')
+      RUBY
+    end
+
+    it 'does not register an offense when using `exists?` with no args' do
+      expect_no_offenses(<<~RUBY)
+        User.exists?
+      RUBY
+    end
+
+    it 'does not register an offense when using `exists?` with an association' do
+      expect_no_offenses(<<~RUBY)
+        user.posts.exists?(published: true)
+      RUBY
+    end
   end
 
-  it 'registers an offense and corrects when using `where(...).exists?` with array argument' do
-    expect_offense(<<~RUBY)
-      User.where(['name = ?', 'john']).exists?
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `exists?(['name = ?', 'john'])` over `where(['name = ?', 'john']).exists?`.
-    RUBY
+  context 'when EnforcedStyle is "where"' do
+    let(:cop_config) { { 'EnforcedStyle' => 'where' } }
 
-    expect_correction(<<~RUBY)
-      User.exists?(['name = ?', 'john'])
-    RUBY
-  end
+    it 'registers an offense and corrects when using `exists?` with a hash' do
+      expect_offense(<<~RUBY)
+        User.exists?(name: 'john')
+             ^^^^^^^^^^^^^^^^^^^^^ Prefer `where(name: 'john').exists?` over `exists?(name: 'john')`.
+      RUBY
 
-  it 'registers an offense and corrects when using `where(...).exists?` with multiple arguments' do
-    expect_offense(<<~RUBY)
-      User.where('name = ?', 'john').exists?
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `exists?(['name = ?', 'john'])` over `where('name = ?', 'john').exists?`.
-    RUBY
+      expect_correction(<<~RUBY)
+        User.where(name: 'john').exists?
+      RUBY
+    end
 
-    expect_correction(<<~RUBY)
-      User.exists?(['name = ?', 'john'])
-    RUBY
-  end
+    it 'registers an offense and corrects when using `exists?` with an array' do
+      expect_offense(<<~RUBY)
+        User.exists?(['name = ?', 'john'])
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `where(['name = ?', 'john']).exists?` over `exists?(['name = ?', 'john'])`.
+      RUBY
 
-  it 'does not register an offense when using `where(...).exists?` with string argument' do
-    expect_no_offenses(<<~RUBY)
-      User.where("name = 'john'").exists?
-    RUBY
-  end
+      expect_correction(<<~RUBY)
+        User.where(['name = ?', 'john']).exists?
+      RUBY
+    end
 
-  it 'does not register an offense when using `exists?`' do
-    expect_no_offenses(<<~RUBY)
-      User.exists?(name: 'john')
-    RUBY
+    it 'registers an offense and corrects when using `exists?` with an multiple arguments' do
+      expect_offense(<<~RUBY)
+        User.exists?('name = ?', 'john')
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `where('name = ?', 'john').exists?` over `exists?('name = ?', 'john')`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        User.where('name = ?', 'john').exists?
+      RUBY
+    end
+
+    it 'registers an offense and corrects when using `exists?` with an association' do
+      expect_offense(<<~RUBY)
+        user.posts.exists?(published: true)
+                   ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `where(published: true).exists?` over `exists?(published: true)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        user.posts.where(published: true).exists?
+      RUBY
+    end
+
+    it 'does not register an offense when using `where(...).exists?` with hash argument' do
+      expect_no_offenses(<<~RUBY)
+        User.where(name: 'john').exists?
+      RUBY
+    end
+
+    it 'does not register an offense when using `where(...).exists?` with array argument' do
+      expect_no_offenses(<<~RUBY)
+        User.where(['name = ?', 'john']).exists?
+      RUBY
+    end
+
+    it 'does not register an offense when using `where(...).exists?` with multiple arguments' do
+      expect_no_offenses(<<~RUBY)
+        User.where('name = ?', 'john').exists?
+      RUBY
+    end
+
+    it 'does not register an offense when using `where(...).exists?` with string argument' do
+      expect_no_offenses(<<~RUBY)
+        User.where("name = 'john'").exists?
+      RUBY
+    end
+
+    it 'does not register an offense when using `where(...).exists?` with an association' do
+      expect_no_offenses(<<~RUBY)
+        user.posts.where(published: true).exists?
+      RUBY
+    end
+
+    it 'does not register an offense when using `exists?` with no args' do
+      expect_no_offenses(<<~RUBY)
+        User.exists?
+      RUBY
+    end
   end
 end
