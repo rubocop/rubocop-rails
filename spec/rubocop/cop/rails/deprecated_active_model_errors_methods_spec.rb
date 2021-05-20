c1453364469c761eac312cd6d652cba53ec91894
lulalala <mchao@gitlab.com>
2021-05-20 20:26:13 +0800

    Add cop to check ActiveModel errors manipulation as hash directly.
    These are deprecated in Rails 6.1 and will be removed in Rails 7.
    See https://github.com/rails/rails/pull/32313 for details.
    
    The cop acts in two modes:
    
    For files under `/models` directory, any `errors` call,
    whether with receiver or not, will be checked.
    For general files, only `errors` calls with receivers will be checked.
    
    E.g. `errors[:bar] = []` is without receiver.
    It will record an offense if it is a model file.
    It will not record an offense if it is other general fie.
    
    This is to reduce false-positives,
    since other classes may also have a `errors` method.

---
+++ spec/rubocop/cop/rails/deprecated_active_model_errors_methods_spec.rb
@@ -0,0 +1,162 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::DeprecatedActiveModelErrorsMethods, :config do
+  shared_examples 'errors call with explicit receiver' do
+    context 'when modifying errors' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY, file_path)
+          user.errors[:name] << 'msg'
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+        RUBY
+      end
+
+      context 'when assigning' do
+        it 'registers an offense' do
+          expect_offense(<<~RUBY, file_path)
+            user.errors[:name] = []
+            ^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+          RUBY
+        end
+      end
+    end
+
+    context 'when modifying errors.messages' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY, file_path)
+          user.errors.messages[:name] << 'msg'
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+        RUBY
+      end
+
+      context 'when assigning' do
+        it 'registers an offense' do
+          expect_offense(<<~RUBY, file_path)
+            user.errors.messages[:name] = []
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+          RUBY
+        end
+      end
+    end
+
+    context 'when modifying errors.details' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY, file_path)
+          user.errors.details[:name] << {}
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+        RUBY
+      end
+
+      context 'when assigning' do
+        it 'registers an offense' do
+          expect_offense(<<~RUBY, file_path)
+            user.errors.details[:name] = []
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+          RUBY
+        end
+      end
+    end
+  end
+
+  shared_examples 'errors call without explicit receiver' do
+    def expect_offense_if_model_file(code, file_path)
+      if file_path.include?('/models/')
+        expect_offense(code, file_path)
+      else
+        code = code.gsub(/^\^+ .+$/, '')
+        expect_no_offenses(code, file_path)
+      end
+    end
+
+    context 'when modifying errors' do
+      it 'registers an offense for model file' do
+        expect_offense_if_model_file(<<~RUBY, file_path)
+          errors[:name] << 'msg'
+          ^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+        RUBY
+      end
+
+      context 'when assigning' do
+        it 'registers an offense' do
+          expect_offense_if_model_file(<<~RUBY, file_path)
+            errors[:name] = []
+            ^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+          RUBY
+        end
+      end
+
+      context 'when calling non-manipulative methods' do
+        it 'does not register an offense' do
+          expect_no_offenses(<<~RUBY, file_path)
+            errors[:name].present?
+          RUBY
+        end
+      end
+    end
+
+    context 'when modifying errors.messages' do
+      it 'registers an offense' do
+        expect_offense_if_model_file(<<~RUBY, file_path)
+          errors.messages[:name] << 'msg'
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+        RUBY
+      end
+
+      context 'when assigning' do
+        it 'registers an offense' do
+          expect_offense_if_model_file(<<~RUBY, file_path)
+            errors.messages[:name] = []
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+          RUBY
+        end
+      end
+
+      context 'when calling non-manipulative methods' do
+        it 'does not register an offense' do
+          expect_no_offenses(<<~RUBY, file_path)
+            errors.messages[:name].present?
+          RUBY
+        end
+      end
+    end
+
+    context 'when modifying errors.details' do
+      it 'registers an offense' do
+        expect_offense_if_model_file(<<~RUBY, file_path)
+          errors.details[:name] << {}
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+        RUBY
+      end
+
+      context 'when assigning' do
+        it 'registers an offense' do
+          expect_offense_if_model_file(<<~RUBY, file_path)
+            errors.details[:name] = []
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid manipulating ActiveModel errors as hash directly.
+          RUBY
+        end
+      end
+
+      context 'when calling non-manipulative methods' do
+        it 'does not register an offense' do
+          expect_no_offenses(<<~RUBY, file_path)
+            errors.details[:name].present?
+          RUBY
+        end
+      end
+    end
+  end
+
+  context 'when file is model file' do
+    let(:file_path) { '/foo/app/models/bar.rb' }
+
+    it_behaves_like 'errors call with explicit receiver'
+    it_behaves_like 'errors call without explicit receiver'
+  end
+
+  context 'when file is generic' do
+    let(:file_path) { '/foo/app/lib/bar.rb' }
+
+    it_behaves_like 'errors call with explicit receiver'
+    it_behaves_like 'errors call without explicit receiver'
+  end
+end
