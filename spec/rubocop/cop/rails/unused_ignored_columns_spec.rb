91da1b4c4b886ab31cab87835a8bd0b5d63914e2
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2021-05-23 13:00:24 +0900

    Add Rails/UnusedIgnoredColumns cop

---
+++ spec/rubocop/cop/rails/unused_ignored_columns_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::UnusedIgnoredColumns, :config do
+  context 'without db/schema.rb' do
+    it 'does nothing' do
+      expect_no_offenses(<<~RUBY)
+        class User < ApplicationRecord
+          self.ignored_columns = [:real_name]
+        end
+      RUBY
+    end
+  end
+
+  context 'with db/schema.rb' do
+    include_context 'with SchemaLoader'
+
+    let(:schema) { <<~RUBY }
+      ActiveRecord::Schema.define(version: 2020_02_02_075409) do
+        create_table "users", force: :cascade do |t|
+          t.string "account", null: false
+        end
+      end
+    RUBY
+
+    context 'with an unused ignored column as a Symbol' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY)
+          class User < ApplicationRecord
+            self.ignored_columns = [:real_name]
+                                    ^^^^^^^^^^ Remove `real_name` from `ignored_columns` because the column does not exist.
+          end
+        RUBY
+      end
+    end
+
+    context 'with an unused ignored column as a String' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY)
+          class User < ApplicationRecord
+            self.ignored_columns = ['real_name']
+                                    ^^^^^^^^^^^ Remove `real_name` from `ignored_columns` because the column does not exist.
+          end
+        RUBY
+      end
+    end
+
+    context 'when ignored_columns= receives existent column as a Symbol' do
+      it 'does nothing' do
+        expect_no_offenses(<<~RUBY)
+          class User < ApplicationRecord
+            self.ignored_columns = [:account]
+          end
+        RUBY
+      end
+    end
+
+    context 'when ignored_columns= receives existent column as a String' do
+      it 'does nothing' do
+        expect_no_offenses(<<~RUBY)
+          class User < ApplicationRecord
+            self.ignored_columns = ['account']
+          end
+        RUBY
+      end
+    end
+
+    context 'when ignored_columns= receives not a literal' do
+      it 'does nothing' do
+        expect_no_offenses(<<~RUBY)
+          class User < ApplicationRecord
+            self.ignored_columns = array
+          end
+        RUBY
+      end
+    end
+  end
+end
---
+++ spec/support/schema_loader.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.shared_context 'with SchemaLoader' do
+  let(:schema_path) do
+    f = Tempfile.create("rubocop-rails-#{described_class}-test-")
+    f.close
+    Pathname(f.path)
+  end
+
+  before do
+    RuboCop::Rails::SchemaLoader.reset!
+    schema_path.write(schema)
+    allow(RuboCop::Rails::SchemaLoader).to receive(:db_schema_path)
+      .and_return(schema_path)
+  end
+
+  after do
+    RuboCop::Rails::SchemaLoader.reset!
+    schema_path.unlink
+  end
+end
