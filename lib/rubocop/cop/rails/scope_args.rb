dfe5e0db31707931c7d4654b320a8bfef0b714f5
Koichi ITO <koic.ito@gmail.com>
2021-07-22 19:47:12 +0900

    Support auto-correction for `Rails/ScopeArgs`
    This PR supports auto-correction for `Rails/ScopeArgs`.

--- lib/rubocop/cop/rails/scope_args.rb
+++ lib/rubocop/cop/rails/scope_args.rb
@@ -14,6 +14,8 @@ module RuboCop
       #   # good
       #   scope :something, -> { where(something: true) }
       class ScopeArgs < Base
+        extend AutoCorrector
+
         MSG = 'Use `lambda`/`proc` instead of a plain method call.'
         RESTRICT_ON_SEND = %i[scope].freeze
 
@@ -21,7 +23,9 @@ module RuboCop
 
         def on_send(node)
           scope?(node) do |second_arg|
-            add_offense(second_arg)
+            add_offense(second_arg) do |corrector|
+              corrector.replace(second_arg, "-> { #{second_arg.source} }")
+            end
           end
         end
       end
