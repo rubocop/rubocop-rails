7d03a57db63e3a89e723082705561f75304a4d21
Niklas Hasselmeyer <niklas.hasselmeyer@makandra.de>
2022-02-10 14:18:30 +0100

    [Fix #565] Add cop Rails/WhereNotWithMultipleConditions
    Rails 6.1 changed how `where.not` works with multiple hash arguments.
    
    This cop identifies places where `where.not` is used with multiple hash
    arguments. It doesn't suggest corrections because depending on the rails
    version, different behaviours may be correct.
    
    ```ruby
    # bad
    User.where.not(trashed: true, role: 'admin')
    
    # good
    User.where.not('trashed = ? OR role = ?', true, 'admin')
    ```

---
+++ spec/rubocop/cop/rails/where_not_with_multiple_conditions_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::WhereNotWithMultipleConditions, :config do
+  it 'does not register an offense for where.not with one condition' do
+    expect_no_offenses(<<~RUBY)
+      User.where.not(trashed: true)
+    RUBY
+  end
+
+  it 'registers an offense for where.not with multiple conditions' do
+    expect_offense(<<~RUBY)
+      User.where.not(trashed: true, role: 'admin')
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use a SQL statement instead of `where.not` with multiple conditions.
+    RUBY
+  end
+
+  it 'registers an offense for where.not with nested multiple conditions' do
+    expect_offense(<<~RUBY)
+      User.joins(:posts).where.not({ posts: { trashed: true, title: 'Rails' } })
+                         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use a SQL statement instead of `where.not` with multiple conditions.
+    RUBY
+  end
+
+  it 'does not register an offense for where with multiple conditions' do
+    expect_no_offenses(<<~RUBY)
+      User.where(trashed: false, role: 'admin')
+    RUBY
+  end
+
+  it 'does not register an offense for where.not with a SQL string' do
+    expect_no_offenses(<<~RUBY)
+      User.where.not('trashed = ? OR role = ?', true, 'admin')
+    RUBY
+  end
+
+  it 'does not register an offense for where.not with one array condition' do
+    expect_no_offenses(<<~RUBY)
+      User.where.not(role: ['moderator', 'admin'])
+    RUBY
+  end
+
+  it 'does not register an offense for chained where.not' do
+    expect_no_offenses(<<~RUBY)
+      User.where.not(trashed: true).where.not(role: 'admin')
+    RUBY
+  end
+end
