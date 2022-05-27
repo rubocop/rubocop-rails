25c2e6b0f9ca965c2575085f09b69b628748d920
Koichi ITO <koic.ito@gmail.com>
2022-05-28 01:58:02 +0900

    [Fix #704] Add new `Rails/StripHeredoc` cop
    Closes #704.
    
    Enforces the use of squiggly heredoc over `strip_heredoc`.
    
    ```ruby
    # bad
    <<EOS.strip_heredoc
      some text
    EOS
    
    # bad
    <<-EOS.strip_heredoc
      some text
    EOS
    
    # good
    <<~EOS
      some text
    EOS
    ```

---
+++ changelog/new_add_new_rails_strip_heredoc_cop.md
@@ -0,0 +1,1 @@
+* [#704](https://github.com/rubocop/rubocop-rails/issues/704): Add new `Rails/StripHeredoc` cop. ([@koic][])
---
+++ spec/rubocop/cop/rails/strip_heredoc_spec.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::StripHeredoc, :config do
+  context 'Ruby <= 2.2', :ruby22 do
+    it 'does not register an offense when using `strip_heredoc`' do
+      expect_no_offenses(<<~RUBY)
+        <<-EOS.strip_heredoc
+          some text
+        EOS
+      RUBY
+    end
+  end
+
+  context 'Ruby >= 2.3', :ruby23 do
+    it 'registers an offense when using `strip_heredoc` with `<<`' do
+      expect_offense(<<~RUBY)
+        <<EOS.strip_heredoc
+        ^^^^^^^^^^^^^^^^^^^ Use squiggly heredoc (`<<~`) instead of `strip_heredoc`.
+          some text
+        EOS
+      RUBY
+
+      expect_correction(<<~RUBY)
+        <<~EOS
+          some text
+        EOS
+      RUBY
+    end
+
+    it 'registers an offense when using `strip_heredoc` with `<<-`' do
+      expect_offense(<<~RUBY)
+        <<-EOS.strip_heredoc
+        ^^^^^^^^^^^^^^^^^^^^ Use squiggly heredoc (`<<~`) instead of `strip_heredoc`.
+          some text
+        EOS
+      RUBY
+
+      expect_correction(<<~RUBY)
+        <<~EOS
+          some text
+        EOS
+      RUBY
+    end
+
+    it 'registers an offense when using `strip_heredoc.do_something`' do
+      expect_offense(<<~RUBY)
+        <<-EOS.strip_heredoc.do_something
+        ^^^^^^^^^^^^^^^^^^^^ Use squiggly heredoc (`<<~`) instead of `strip_heredoc`.
+          some text
+        EOS
+      RUBY
+
+      expect_correction(<<~RUBY)
+        <<~EOS.do_something
+          some text
+        EOS
+      RUBY
+    end
+
+    it 'registers an offense when using `strip_heredoc` with multiline text' do
+      expect_offense(<<~RUBY)
+        <<-EOS.strip_heredoc
+        ^^^^^^^^^^^^^^^^^^^^ Use squiggly heredoc (`<<~`) instead of `strip_heredoc`.
+          some text
+          some text
+        EOS
+      RUBY
+
+      expect_correction(<<~RUBY)
+        <<~EOS
+          some text
+          some text
+        EOS
+      RUBY
+    end
+
+    it 'does not register an offense when using squiggly heredoc' do
+      expect_no_offenses(<<~RUBY)
+        <<~EOS
+          some text
+        EOS
+      RUBY
+    end
+
+    it 'does not register an offense when using `do_something.strip_heredoc`' do
+      expect_no_offenses(<<~RUBY)
+        <<-EOS.do_something.strip_heredoc
+          some text
+        EOS
+      RUBY
+    end
+
+    it 'does not register an offense when using `strip_heredoc` without receiver' do
+      expect_no_offenses(<<~RUBY)
+        strip_heredoc
+      RUBY
+    end
+  end
+end
