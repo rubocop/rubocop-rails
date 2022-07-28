2baf74478db35f860f233507c7969a8ff2722b28
Ryo Nakamura <r7kamura@gmail.com>
2022-07-29 07:49:25 +0900

    Add `Rails/TopLevelHashWithIndifferentAccess` cop

---
+++ spec/rubocop/cop/rails/top_level_hash_with_indifferent_access_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::TopLevelHashWithIndifferentAccess, :config, :rails51 do
+  context 'with top-level HashWithIndifferentAccess' do
+    it 'registers and corrects an offense' do
+      expect_offense(<<~RUBY)
+        HashWithIndifferentAccess.new(foo: 'bar')
+        ^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid top-level `HashWithIndifferentAccess`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ActiveSupport::HashWithIndifferentAccess.new(foo: 'bar')
+      RUBY
+    end
+  end
+
+  context 'with top-level ::HashWithIndifferentAccess' do
+    it 'registers and corrects an offense' do
+      expect_offense(<<~RUBY)
+        ::HashWithIndifferentAccess.new(foo: 'bar')
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid top-level `HashWithIndifferentAccess`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        ::ActiveSupport::HashWithIndifferentAccess.new(foo: 'bar')
+      RUBY
+    end
+  end
+
+  context 'with ActiveSupport::HashWithIndifferentAccess' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        ActiveSupport::HashWithIndifferentAccess.new(foo: 'bar')
+      RUBY
+    end
+  end
+
+  context 'with ActiveSupport::HashWithIndifferentAccess on Rails 5.0', :rails50 do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        HashWithIndifferentAccess.new(foo: 'bar')
+      RUBY
+    end
+  end
+end
