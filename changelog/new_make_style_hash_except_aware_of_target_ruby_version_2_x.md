8a619644e30ada918fc097a99f16db04d35db278
Koichi ITO <koic.ito@gmail.com>
2022-09-23 03:29:09 +0900

    [Fix #790] Make `Style/HashExcept` aware of TargetRubyVersion: 2.x
    Fixes #790.
    
    This PR makes `Style/HashExcept` aware of TargetRubyVersion: 2.x
    because Rails has `Hash#except`.
    So Ruby version is irrelevant when used in Rails.

---
+++ changelog/new_make_style_hash_except_aware_of_target_ruby_version_2_x.md
@@ -0,0 +1,1 @@
+* [#790](https://github.com/rubocop/rubocop-rails/issues/790): Make `Style/HashExcept` aware of TargetRubyVersion: 2.x because Rails has `Hash#except`. ([@koic][])
--- spec/rubocop/cli/autocorrect_spec.rb
+++ spec/rubocop/cli/autocorrect_spec.rb
@@ -40,4 +40,20 @@ RSpec.describe 'RuboCop::CLI --autocorrect', :isolated_environment do # rubocop:
       self&.bar&.baz
     RUBY
   end
+
+  it 'corrects `Style/HashExcept` with `TargetRubyVersion: 2.0`' do
+    create_file('.rubocop.yml', <<~YAML)
+      AllCops:
+        TargetRubyVersion: 2.0
+    YAML
+
+    create_file('example.rb', <<~RUBY)
+      {foo: 1, bar: 2, baz: 3}.reject {|k, v| k == :bar }
+    RUBY
+
+    expect(cli.run(['-a', '--only', 'Style/HashExcept'])).to eq(0)
+    expect(File.read('example.rb')).to eq(<<~RUBY)
+      {foo: 1, bar: 2, baz: 3}.except(:bar)
+    RUBY
+  end
 end
