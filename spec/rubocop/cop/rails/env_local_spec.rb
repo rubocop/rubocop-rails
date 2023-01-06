e0cb140f22349c3ce8f897123f1b7269895bf0ad
Sam Bostock <sam.bostock@shopify.com>
2023-01-06 00:22:58 -0500

    Add `Rails/EnvLocal` cop
    Enforces the use of `Rails.env.local?` instead of
    `Rails.env.development? || Rails.env.test?`, as of Rails 7.1.

---
+++ spec/rubocop/cop/rails/env_local_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::EnvLocal, :config do
+  shared_examples 'non-local candidates' do
+    it 'registers no offenses for non-local `Rails.env._? || Rails.env._?`' do
+      expect_no_offenses(<<~RUBY)
+        Rails.env.development? || Rails.env.production?
+        Rails.env.test? || Rails.env.production?
+        Rails.env.production? || Rails.env.other?
+      RUBY
+    end
+
+    it 'registers no offenses for single `Rails.env._?`' do
+      expect_no_offenses(<<~RUBY)
+        Rails.env.development?
+        Rails.env.test?
+        Rails.env.production?
+        Rails.env.other?
+      RUBY
+    end
+  end
+
+  context 'In Rails >= 7.1', :rails71 do
+    it 'registers an offense for `Rails.env.development? || Rails.env.test?`' do
+      expect_offense(<<~RUBY)
+        Rails.env.development? || Rails.env.test?
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `Rails.env.local?` instead.
+        Rails.env.test? || Rails.env.development?
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `Rails.env.local?` instead.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        Rails.env.local?
+        Rails.env.local?
+      RUBY
+    end
+
+    it 'registers no offenses for `Rails.env.local?`' do
+      expect_no_offenses(<<~RUBY)
+        Rails.env.local?
+      RUBY
+    end
+
+    include_examples 'non-local candidates'
+  end
+
+  context 'In Rails < 7.1', :rails70 do
+    it 'registers no offenses for `Rails.env.development? || Rails.env.test?`' do
+      expect_no_offenses(<<~RUBY)
+        Rails.env.development? || Rails.env.test?
+        Rails.env.test? || Rails.env.development?
+      RUBY
+    end
+
+    it 'registers no offenses for `Rails.env.local?`' do
+      expect_no_offenses(<<~RUBY)
+        Rails.env.local?
+      RUBY
+    end
+
+    include_examples 'non-local candidates'
+  end
+end
