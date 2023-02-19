b74740a7e0ea3f9aa3a360980bb45e904899057d
Koichi ITO <koic.ito@gmail.com>
2023-02-19 16:50:43 +0900

    Make `Style/InverseMethods` aware of some Active Support methods
    This PR makes `Style/InverseMethods` aware of Active Support's
    `present?`, `blank?`, `include?`, and `exclude?` methods.
    
    These methods are based on the following setting:
    https://github.com/rubocop/rubocop/blob/v1.45.0/config/default.yml#L4030-L4033

---
+++ changelog/change_make_style_inverse_methods_aware_of_active_support_methods.md
@@ -0,0 +1,1 @@
+* [#935](https://github.com/rubocop/rubocop-rails/pull/935): Make `Style/InverseMethods` aware of Active Support's `present?`, `blank?`, `include?`, and `exclude?` methods. ([@koic][])
--- lib/rubocop/rails.rb
+++ lib/rubocop/rails.rb
@@ -5,7 +5,7 @@ module RuboCop
   module Rails
     PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
     CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
-    CONFIG         = YAML.safe_load(CONFIG_DEFAULT.read).freeze
+    CONFIG         = YAML.safe_load(CONFIG_DEFAULT.read, permitted_classes: [Regexp, Symbol]).freeze
 
     private_constant(:CONFIG_DEFAULT, :PROJECT_ROOT)
 
