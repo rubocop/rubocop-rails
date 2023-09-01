5043655b1be444bcd74f2e6be864606e626cac16
Koichi ITO <koic.ito@gmail.com>
2023-09-02 04:19:40 +0900

    [Fix #1075] Add new `Rails/SelectMap` cop
    Resolves #1075.
    
    This PR adds new `Rails/SelectMap` cop that checks for uses of `select(:column_name)` with `map(&:column_name)`.
    These can be replaced with `pluck(:column_name)`.
    There also should be some performance improvement since it skips instantiating the model class for matches.
    
    ```ruby
    # bad
    Model.select(:column_name).map(&:column_name)
    
    # good
    Model.pluck(:column_name)
    ```

---
+++ spec/rubocop/cop/rails/select_map_spec.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::SelectMap, :config do
+  it 'registers an offense when using `select(:column_name).map(&:column_name)`' do
+    expect_offense(<<~RUBY)
+      Model.select(:column_name).map(&:column_name)
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `pluck(:column_name)` instead of `select` with `map`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Model.pluck(:column_name)
+    RUBY
+  end
+
+  it "registers an offense when using `select('column_name').map(&:column_name)`" do
+    expect_offense(<<~RUBY)
+      Model.select('column_name').map(&:column_name)
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `pluck(:column_name)` instead of `select` with `map`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Model.pluck(:column_name)
+    RUBY
+  end
+
+  it 'registers an offense when using `select(:column_name).collect(&:column_name)`' do
+    expect_offense(<<~RUBY)
+      Model.select(:column_name).collect(&:column_name)
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `pluck(:column_name)` instead of `select` with `collect`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Model.pluck(:column_name)
+    RUBY
+  end
+
+  it 'registers an offense when using `select(:column_name).where(conditions).map(&:column_name)`' do
+    expect_offense(<<~RUBY)
+      Model.select(:column_name).where(conditions).map(&:column_name)
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `pluck(:column_name)` instead of `select` with `map`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      Model.where(conditions).pluck(:column_name)
+    RUBY
+  end
+
+  it 'does not register an offense when using `select(:mismatch_column_name).map(&:column_name)`' do
+    expect_no_offenses(<<~RUBY)
+      Model.select(:mismatch_column_name).map(&:column_name)
+    RUBY
+  end
+
+  it 'does not register an offense when using `select(:column_name, :other_column_name).map(&:column_name)`' do
+    expect_no_offenses(<<~RUBY)
+      Model.select(:column_name, :other_column_name).map(&:column_name)
+    RUBY
+  end
+
+  it 'does not register an offense when using `select(column_names).map(&:column_name)`' do
+    expect_no_offenses(<<~RUBY)
+      Model.select(column_names).map(&:column_name)
+    RUBY
+  end
+
+  it 'does not register an offense when using `select(:column_name).do_something(&:column_name)`' do
+    expect_no_offenses(<<~RUBY)
+      Model.select(:column_name).do_something(&:column_name)
+    RUBY
+  end
+
+  it 'does not register an offense when using `select { |item| item.column_name }.map(&:column_name)`' do
+    expect_no_offenses(<<~RUBY)
+      Model.select { |item| item.column_name }.map(&:column_name)
+    RUBY
+  end
+
+  it 'does not register an offense when using `select(:column_name).map { |item| do_something(item) }`' do
+    expect_no_offenses(<<~RUBY)
+      Model.select(:column_name).map { |item| do_something(item) }
+    RUBY
+  end
+
+  it 'does not register an offense when using `pluck(:column_name)`' do
+    expect_no_offenses(<<~RUBY)
+      Model.pluck(:column_name)
+    RUBY
+  end
+end
