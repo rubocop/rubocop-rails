8773d378e30ec1d987a5ed12c03e3eb9dd8cc2ba
r7kamura <r7kamura@gmail.com>
2023-11-10 21:26:46 +0900

    Support `Nokogiri::HTML.parse` and `Nokogiri::HTML5.parse` on `Rails/ResponseParsedBody`

--- lib/rubocop/cop/rails/response_parsed_body.rb
+++ lib/rubocop/cop/rails/response_parsed_body.rb
@@ -3,24 +3,29 @@
 module RuboCop
   module Cop
     module Rails
-      # Prefer `response.parsed_body` to `JSON.parse(response.body)`.
+      # Prefer `response.parsed_body` to custom parsing logic for `response.body`.
       #
       # @safety
-      #   This cop is unsafe because Content-Type may not be `application/json`. For example, the proprietary
-      #   Content-Type provided by corporate entities such as `application/vnd.github+json` is not supported at
-      #   `response.parsed_body` by default, so you still have to use `JSON.parse(response.body)` there.
+      #   This cop is unsafe because Content-Type may not be `application/json` or `text/html`.
+      #   For example, the proprietary Content-Type provided by corporate entities such as
+      #   `application/vnd.github+json` is not supported at `response.parsed_body` by default,
+      #   so you still have to use `JSON.parse(response.body)` there.
       #
       # @example
       #   # bad
       #   JSON.parse(response.body)
+      #
+      #   # bad
+      #   Nokogiri::HTML.parse(response.body)
+      #
+      #   # bad
+      #   Nokogiri::HTML5.parse(response.body)
       #
       #   # good
       #   response.parsed_body
       class ResponseParsedBody < Base
         extend AutoCorrector
         extend TargetRailsVersion
-
-        MSG = 'Prefer `response.parsed_body` to `JSON.parse(response.body)`.'
 
         RESTRICT_ON_SEND = %i[parse].freeze
 
@@ -38,18 +43,55 @@ module RuboCop
           )
         PATTERN
 
-        def on_send(node)
-          return unless json_parse_response_body?(node)
+        # @!method nokogiri_html_parse_response_body(node)
+        def_node_matcher :nokogiri_html_parse_response_body, <<~PATTERN
+          (send
+            (const
+              (const {nil? cbase} :Nokogiri)
+              ${:HTML :HTML5}
+            )
+            :parse
+            (send
+              (send nil? :response)
+              :body
+            )
+          )
+        PATTERN
 
-          add_offense(node) do |corrector|
-            autocorrect(corrector, node)
-          end
+        def on_send(node)
+          check_json_parse_response_body(node)
+
+          return unless target_rails_version >= 7.1
+
+          check_nokogiri_html_parse_response_body(node)
         end
 
         private
 
         def autocorrect(corrector, node)
           corrector.replace(node, 'response.parsed_body')
+        end
+
+        def check_json_parse_response_body(node)
+          return unless json_parse_response_body?(node)
+
+          add_offense(
+            node,
+            message: 'Prefer `response.parsed_body` to `JSON.parse(response.body)`.'
+          ) do |corrector|
+            autocorrect(corrector, node)
+          end
+        end
+
+        def check_nokogiri_html_parse_response_body(node)
+          return unless (const = nokogiri_html_parse_response_body(node))
+
+          add_offense(
+            node,
+            message: "Prefer `response.parsed_body` to `Nokogiri::#{const}.parse(response.body)`."
+          ) do |corrector|
+            autocorrect(corrector, node)
+          end
         end
       end
     end
--- spec/rubocop/cop/rails/response_parsed_body_spec.rb
+++ spec/rubocop/cop/rails/response_parsed_body_spec.rb
@@ -42,4 +42,38 @@ RSpec.describe RuboCop::Cop::Rails::ResponseParsedBody, :config do
       RUBY
     end
   end
+
+  context 'when `Nokogiri::HTML.parse(response.body)` is used on Rails 7.0', :rails70 do
+    it 'registers no offense' do
+      expect_no_offenses(<<~RUBY)
+        Nokogiri::HTML.parse(response.body)
+      RUBY
+    end
+  end
+
+  context 'when `Nokogiri::HTML.parse(response.body)` is used on Rails 7.1', :rails71 do
+    it 'registers offense' do
+      expect_offense(<<~RUBY)
+        Nokogiri::HTML.parse(response.body)
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `response.parsed_body` to `Nokogiri::HTML.parse(response.body)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        response.parsed_body
+      RUBY
+    end
+  end
+
+  context 'when `Nokogiri::HTML5.parse(response.body)` is used on Rails 7.1', :rails71 do
+    it 'registers offense' do
+      expect_offense(<<~RUBY)
+        Nokogiri::HTML5.parse(response.body)
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `response.parsed_body` to `Nokogiri::HTML5.parse(response.body)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        response.parsed_body
+      RUBY
+    end
+  end
 end
