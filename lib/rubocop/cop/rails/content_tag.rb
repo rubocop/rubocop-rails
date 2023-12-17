1fe638532c16d32e4169ebce889add346836fd28
Koichi ITO <koic.ito@gmail.com>
2023-12-17 16:39:28 +0800

    [Fix #1218] Fix a typo
    Follow up https://github.com/rubocop/rubocop-rails/pull/526

--- lib/rubocop/cop/rails/content_tag.rb
+++ lib/rubocop/cop/rails/content_tag.rb
@@ -7,7 +7,7 @@ module RuboCop
       #
       # NOTE: Allow `tag` when the first argument is a variable because
       # `tag(name)` is simpler rather than `tag.public_send(name)`.
-      # And this cop will be renamed to something like `LegacyTag` in the future. (e.g. RuboCop Rails 2.0)
+      # And this cop will be renamed to something like `LegacyTag` in the future. (e.g. RuboCop Rails 3.0)
       #
       # @example
       #  # bad
