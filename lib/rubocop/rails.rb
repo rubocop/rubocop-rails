1eacbc30ec427e8236803036da4007f5cfda885c
Koichi ITO <koic.ito@gmail.com>
2025-01-08 16:33:12 +0900

    Remove a useless constant
    It seems that unnecessary constant was left behind when introducing the Inject mechanism,
    inspired by RuboCop RSpec, during the initial implementation of this gem.

--- lib/rubocop/rails.rb
+++ lib/rubocop/rails.rb
@@ -5,7 +5,6 @@ module RuboCop
   module Rails
     PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
     CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
-    CONFIG         = YAML.safe_load(CONFIG_DEFAULT.read, permitted_classes: [Regexp, Symbol]).freeze
 
     private_constant(:CONFIG_DEFAULT, :PROJECT_ROOT)
 
