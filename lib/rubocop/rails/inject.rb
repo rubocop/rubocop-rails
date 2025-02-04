eb736486e792095099b7372549af5fe57931b54e
Koichi ITO <koic.ito@gmail.com>
2025-02-04 10:54:20 +0900

    Avoid unnecessary `send`

--- lib/rubocop/rails/inject.rb
+++ lib/rubocop/rails/inject.rb
@@ -7,7 +7,7 @@ module RuboCop
     module Inject
       def self.defaults!
         path = CONFIG_DEFAULT.to_s
-        hash = ConfigLoader.send(:load_yaml_configuration, path)
+        hash = ConfigLoader.load_yaml_configuration(path)
         config = Config.new(hash, path).tap(&:make_excludes_absolute)
         puts "configuration from #{path}" if ConfigLoader.debug?
         config = ConfigLoader.merge_with_default(config, path, unset_nil: false)
