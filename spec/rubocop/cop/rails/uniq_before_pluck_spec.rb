198068155fe2e79a784c0cc4d6c889140d1f27dc
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-02-18 13:40:58 +0100

    [Fix #1427] Fix an error for `Rails/UniqBeforePluck` with multi-line expression
    This introduces a bit of whitespace when the expression spans multiple lines, but that seesm fine

--- spec/rubocop/cop/rails/uniq_before_pluck_spec.rb
+++ spec/rubocop/cop/rails/uniq_before_pluck_spec.rb
@@ -37,6 +37,20 @@ RSpec.describe RuboCop::Cop::Rails::UniqBeforePluck, :config do
       RUBY
     end
 
+    it 'corrects when uniq and pluck are on different lines' do
+      expect_offense(<<~RUBY)
+        Model
+          .pluck(:name)
+          .uniq
+           ^^^^ Use `distinct` before `pluck`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        Model
+          .distinct.pluck(:name)
+      RUBY
+    end
+
     it 'ignores uniq before pluck' do
       expect_no_offenses(<<~RUBY)
         Model.where(foo: 1).uniq.pluck(:something)
@@ -83,6 +97,12 @@ RSpec.describe RuboCop::Cop::Rails::UniqBeforePluck, :config do
     it 'ignores uniq on an association' do
       expect_no_offenses(<<~RUBY)
         instance.assoc.pluck(:name).uniq
+      RUBY
+    end
+
+    it 'ignores uniq without an receiver' do
+      expect_no_offenses(<<~RUBY)
+        pluck(:name).uniq
       RUBY
     end
   end
