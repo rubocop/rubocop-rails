5690b95894ae80e526791feba228d0aeb9658d95
Adam Stegman <astegman@onemedical.com>
2025-10-08 16:33:09 +0000

    Add `abort` to the `Rails/Exit` cop
    `abort` is very similar to `exit` in Ruby
    ([docs](https://docs.ruby-lang.org/en/3.4/Kernel.html#method-i-abort)),
    but it takes a string message or Exception. Rails applications should
    avoid its usage for the same reasons as they avoid `exit` and `exit!`.
    Since they are used in the same way, I thought it belonged in the same
    cop, but am glad to move it to a new one if that's preferred.

--- spec/rubocop/cop/rails/exit_spec.rb
+++ spec/rubocop/cop/rails/exit_spec.rb
@@ -4,14 +4,21 @@ RSpec.describe RuboCop::Cop::Rails::Exit, :config do
   it 'registers an offense for an exit call with no receiver' do
     expect_offense(<<~RUBY)
       exit
-      ^^^^ Do not use `exit` in Rails applications.
+      ^^^^ Do not use `exit` or `abort` in Rails applications.
     RUBY
   end
 
   it 'registers an offense for an exit! call with no receiver' do
     expect_offense(<<~RUBY)
       exit!
-      ^^^^^ Do not use `exit` in Rails applications.
+      ^^^^^ Do not use `exit` or `abort` in Rails applications.
+    RUBY
+  end
+
+  it 'registers an offense for an abort call with no receiver' do
+    expect_offense(<<~RUBY)
+      abort
+      ^^^^^ Do not use `exit` or `abort` in Rails applications.
     RUBY
   end
 
@@ -27,18 +34,33 @@ RSpec.describe RuboCop::Cop::Rails::Exit, :config do
     it 'does not register an offense for an explicit exit! call on an object' do
       expect_no_offenses('Object.new.exit!(0)')
     end
+
+    it 'does not register an offense for an explicit abort call on an object' do
+      expect_no_offenses('Object.new.abort("failed")')
+    end
   end
 
   context 'with arguments' do
     it 'registers an offense for an exit(0) call with no receiver' do
       expect_offense(<<~RUBY)
         exit(0)
-        ^^^^ Do not use `exit` in Rails applications.
+        ^^^^ Do not use `exit` or `abort` in Rails applications.
       RUBY
     end
 
     it 'ignores exit calls with unexpected number of parameters' do
       expect_no_offenses('exit(1, 2)')
+    end
+
+    it 'registers an offense for an abort("message") call with no receiver' do
+      expect_offense(<<~RUBY)
+        abort("message")
+        ^^^^^ Do not use `exit` or `abort` in Rails applications.
+      RUBY
+    end
+
+    it 'ignores abort calls with unexpected number of parameters' do
+      expect_no_offenses('abort("message", "another message")')
     end
   end
 
@@ -46,14 +68,28 @@ RSpec.describe RuboCop::Cop::Rails::Exit, :config do
     it 'does register an offense for explicit Kernel.exit calls' do
       expect_offense(<<~RUBY)
         Kernel.exit
-               ^^^^ Do not use `exit` in Rails applications.
+               ^^^^ Do not use `exit` or `abort` in Rails applications.
       RUBY
     end
 
     it 'does register an offense for explicit Process.exit calls' do
       expect_offense(<<~RUBY)
         Process.exit
-                ^^^^ Do not use `exit` in Rails applications.
+                ^^^^ Do not use `exit` or `abort` in Rails applications.
+      RUBY
+    end
+
+    it 'does register an offense for explicit Kernel.abort calls' do
+      expect_offense(<<~RUBY)
+        Kernel.abort
+               ^^^^^ Do not use `exit` or `abort` in Rails applications.
+      RUBY
+    end
+
+    it 'does register an offense for explicit Process.abort calls' do
+      expect_offense(<<~RUBY)
+        Process.abort
+                ^^^^^ Do not use `exit` or `abort` in Rails applications.
       RUBY
     end
   end
